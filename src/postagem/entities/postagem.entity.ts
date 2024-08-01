64bdb3fb1e5549363b690f5baaa1ea334556dce1
Fernando Cassio <fernandocassiodev@gmail.com>
2024-07-31 21:13:26 -0300

    refactor: relaciona a entidade postagem com usuario

--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -2,8 +2,9 @@ import { Transform, TransformFnParams } from "class-transformer";
 import { IsNotEmpty } from "class-validator";
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { Tema } from "../../tema/entities/tema.entity";
+import { Usuario } from "../../usuario/entities/usuario.entity";
 
-@Entity({name: "tb_postagens"})
+@Entity({ name: "tb_postagens" })
 export class Postagem {
 
     @PrimaryGeneratedColumn() //Chave primária Autoimcremental
@@ -11,16 +12,21 @@ export class Postagem {
 
     @Transform(({ value }: TransformFnParams) => value?.trim()) // verificar se o titulo não é um espaço vazio
     @IsNotEmpty() //não aceitar titulo vazio
-    @Column({length: 100, nullable: false}) // definir o tamanho e não aceitar número
+    @Column({ length: 100, nullable: false }) // definir o tamanho e não aceitar número
     titulo: string;
 
     @Transform(({ value }: TransformFnParams) => value?.trim())// verificar se o texto não é um espaço vazio
     @IsNotEmpty()
-    @Column({length: 1000, nullable: false})
+    @Column({ length: 1000, nullable: false })
     texto: string;
 
     @UpdateDateColumn() //A data e a hora serão preenchidas automaticamente
     data: Date;
+
+    @ManyToOne(() => Usuario, (usuario) => usuario.postagem, {
+        onDelete: 'CASCADE'
+    })
+    usuario: Usuario
 
     //muitos para um,ou seja, muitas postagens possuem um tema
     @ManyToOne(() => Tema, (tema) => tema.postagem, {
